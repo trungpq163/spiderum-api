c36d9b89f43178b80fa0964edf8c1221f37fc5c0
trungpq163 <miyucoder@gmail.com>
2021-11-17 17:02:17 +0700

    Add some config to live vercel & Add test API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import test from './api/test';
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
+
+app.use('/test', test);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
