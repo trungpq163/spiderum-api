fe482ed504a1e51582e174cc8caa3521e3229c4e
trungpq163 <miyucoder@gmail.com>
2021-12-25 05:55:55 +0700

    Update get post details API

--- src/services/post-details/index.ts
+++ src/services/post-details/index.ts
@@ -1,12 +1,23 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { IGetPostDetails } from '../../types';
 
-export const getPostDetails = ({ slug }: IGetPostDetails) => {
+export const getPostDetailsThroughRawHTMLService = ({
+  slug,
+}: IGetPostDetails) => {
   const config: AxiosRequestConfig = {
     method: 'get',
     url: `${process.env.SPIDERUM_TARGET_URL}/bai-dang/${slug}`,
     headers: {},
   };
 
+  return axios(config);
+};
+
+export const getPostDetailsService = ({ slug }: IGetPostDetails) => {
+  const config: AxiosRequestConfig = {
+    method: 'get',
+    url: `${process.env.SPIDERUM_API_V1_URL}/post/${slug}`,
+    headers: {},
+  };
   return axios(config);
 };
---
+++ src/utils/removeVietNameseAccent.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-useless-escape */
+export const stringToSlug = (str: string): string => {
+  // remove accents
+  const from =
+      'àáãảạăằắẳẵặâầấẩẫậèéẻẽẹêềếểễệđùúủũụưừứửữựòóỏõọôồốổỗộơờớởỡợìíỉĩịäëïîöüûñçýỳỹỵỷ',
+    to =
+      'aaaaaaaaaaaaaaaaaeeeeeeeeeeeduuuuuuuuuuuoooooooooooooooooiiiiiaeiiouuncyyyyy';
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(RegExp(from[i], 'gi'), to[i]);
+  }
+
+  str = str
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\-]/g, '-')
+    .replace(/-+/g, '-');
+
+  return str;
+};
